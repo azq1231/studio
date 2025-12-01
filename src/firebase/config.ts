904e9d5f49ed7fe86cd4ddcee4c3e772115bdd2e
music char <cnmusic7@gmail.com>
2025-12-01 15:39:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2399006184-77f24",
+  "appId": "1:230110380303:web:9e30b5be430f1473f37e7a",
+  "apiKey": "AIzaSyA1P9PsJrBMZD1DtW5NKvkD-vw-QYlKeW0",
+  "authDomain": "studio-2399006184-77f24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "230110380303"
+};
